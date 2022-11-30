aaa5c9eb0ca5b916b30e6c6d7dc95d7cb1a87884
Sarvesh <sarvesh.moraskar@gmail.com>
2022-11-30 17:52:17 +0530

    Local connection of Nodejs with MongoDB

---
+++ mongoLocal.js
@@ -0,0 +1,15 @@
+//Nodejs mongodb connection and create database
+
+const MongoClient = require('mongodb').MongoClient; //Mongo client required to connect to local instance
+var url = "mongodb://127.0.0.1:27017/employee"; //default url for mongo
+
+MongoClient.connect(url,function(err,db){
+    if (err) throw err;
+    console.log("Database created!");
+    var dbo = db.db("nodedb");
+    dbo.createCollection("Student",function(err,res){
+        if (err) throw err;
+        console.log("Collection created!");
+        db.close();
+    });
+});
